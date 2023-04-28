f1cece158ce2ee71fb13359056dd226432a4824b
Oleksiy Koren <firithe@gmail.com>
2023-04-28 23:09:55 +0200

    Document the code

--- src/config/zeromq.config.js
+++ src/config/zeromq.config.js
@@ -1,17 +1,22 @@
-
 import zmq from "zeromq";
 
 const ZMQ_ADDRESS = "tcp://127.0.0.1:5556";
 
+/**
+ * Binds a ZeroMQ socket to the specified address.
+ * @returns {ZeroMQ.Socket} - A ZeroMQ socket instance.
+ */
 async function bindZeroMQSocket() {
   const sock = zmq.socket("push");
-  console.log("Starting producer on", ZMQ_ADDRESS);
-  await sock.bind(ZMQ_ADDRESS);
-  console.log("Producer bound to", ZMQ_ADDRESS);
+
+  // Set up the ZeroMQ socket
+  const endpoint = process.env.ZEROMQ_ENDPOINT || ZMQ_ADDRESS;
+  console.log("Starting producer on", endpoint);
+  await sock.connect(endpoint);
+
+  console.log("Producer bound to", endpoint);
 
   return sock;
 }
 
-export {
-  bindZeroMQSocket,
-};
+export { bindZeroMQSocket };
